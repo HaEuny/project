08a58c0c45339023b8388db408e16e85d1d78226
USER <USER@DESKTOP-0NES1F9>
2019-12-31 10:29:46 +0900

    MemberBean.java 추가
    Member작업하기 위해

--- ITLearnSite/src/member/db/MemberBean.java
+++ ITLearnSite/src/member/db/MemberBean.java
@@ -1,5 +1,16 @@
 package member.db;
 
 public class MemberBean {
-	/* MemberBean �ۼ� �ٶ� */
+	String email;
+	String pw;
+	String name;
+	int gender;
+	String birth_year;
+	String birth_month;
+	String birth_day;
+	String phonenumber;
+	String address;
+	String address1;
+	String address2;
+	int sms;
 }
