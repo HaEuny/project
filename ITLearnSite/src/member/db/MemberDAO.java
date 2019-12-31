fd860cf93b74861bde9b417d773376320dc905d5
khr5580 <59347930+khr5580@users.noreply.github.com>
2019-12-31 13:43:25 +0900

    Update MemberDAO.java

--- ITLearnSite/src/member/db/MemberDAO.java
+++ ITLearnSite/src/member/db/MemberDAO.java
@@ -1,8 +1,31 @@
 package member.db;
 
 public class MemberDAO implements MemberDAOInfc{
-
-	
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+    DataSource ds = null;
+    String sql = "";
+    Statement stmt = null;
+    //DB연결
+    private Connection getConnection() throws Exception {
+        Context ctx = new InitialContext();
+        ds = (DataSource)ctx.lookup("java:comp/env/jdbc/jspbeginner");
+        return ds.getConnection();
+    }
+    
+    //DB연결해제
+    private void closeConnection(){
+        try {
+            if(rs != null) rs.close();
+            if(pstmt != null) pstmt.close();
+            if(con != null) con.close();
+            if(stmt != null) stmt.close();
+          
+        } catch (SQLException e) {
+            System.out.println("closeConnection()메소드에서 오류  : " +e);
+        }
+    }
 	@Override
 	public void deleteMember() {
 		
